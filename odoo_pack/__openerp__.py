61cbbb9ce9305389721f9162b652d58551ba8e4d
Gabriel Lopez Alarcon <gbolpz@gmail.com>
2017-07-12 13:42:45 -0500

    Pack Version 10

---
+++ odoo_pack/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Paquete',
+    'author': 'Gabriel Lopez',
+    'version' : '1',
+    'summary': 'Paquetes y Productos',
+    'sequence': 30,
+    'description': """
+
+
+
+    """,
+    'category': 'Productos',
+    'website': 'www.argil.mx',
+    'images' : [],
+    'depends' : ['sale','account',],
+    'data': [
+        'pack.xml',
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ odoo_pack/pack.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _
+from openerp.exceptions import ValidationError
+
+class sale_order_line(models.Model):
+    _name = 'sale.order.line'
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def create(self, values):
+        res = super(sale_order_line,self).create(values)
+        if res.product_id.pack == True:
+            if res.product_id.tip == False:
+                for xpak in res.product_id.pro_ids:
+                    values.update({'product_id':xpak.pack_pro_id.id,'name':str(res.product_id.name)+" - "+str(xpak.pack_pro_id.name),'price_unit':0,'product_uom_qty':xpak.subtotal_price*res.product_uom_qty})
+                    self.create(values)
+            if res.product_id.tip == True:
+                for xpak in res.product_id.pro_ids:
+                    values.update({'product_id':xpak.pack_pro_id.id,'name':str(res.product_id.name)+" - "+str(xpak.pack_pro_id.name),'price_unit':xpak.pack_pro_id.list_price,'product_uom_qty':xpak.subtotal_price*res.product_uom_qty})
+                    self.create(values)
+        return res
+
+class pack_product(models.Model):
+    _name = 'pack.product'
+    _description = 'Productos de Paquete'
+    pro_id = fields.Many2one('product.template', 'Referencia')
+    uni_med = fields.Many2one('product.uom', 'Unidad de medida')
+    qty = fields.Float('Cantidad', digits=(3,0), default=1)
+    pack_pro_id = fields.Many2one('product.template', 'Producto')
+    subtotal_price = fields.Float('Qty', digits=(14,3), compute="calcula_total")
+    subtotal = fields.Float('Subtotal', digits=(14,3), readonly=True)
+
+    @api.depends('uni_med', 'qty', 'pack_pro_id')
+    @api.one
+    def calcula_total(self):
+        if self.uni_med.uom_type == 'bigger':
+            self.subtotal_price = self.uni_med.factor_inv*self.qty
+            print "####### SUBTOTAL BIGGER >>>>>>>> ", self.subtotal_price
+        if self.uni_med.uom_type == 'reference':
+            self.subtotal_price = self.qty
+            print "####### SUBTOTAL REFERENCE >>>>>>>> ", self.subtotal_price
+        if self.uni_med.uom_type == 'smaller':
+            self.subtotal_price = self.qty/self.uni_med.factor
+            print "####### SUBTOTAL SMALLER >>>>>>>> ", self.subtotal_price
+
+    @api.onchange('pack_pro_id', 'uni_med', 'qty')
+    def calcula_subtotal(self):
+        self.subtotal = self.pack_pro_id.list_price*self.subtotal_price
+
+    @api.constrains('qty')
+    @api.one
+    def _check_qty(self):
+        if self.qty == 0:
+            raise ValidationError(_('Error en la Cantidad del Producto:\n %s \nLa Cantidad del Producto No Debe Tener Valor "0".\nSi Desea Puede Eliminar el Producto del Paquete'%self.pack_pro_id.name))
+
+    @api.onchange('pack_pro_id')
+    def onchange_uni_med(self):
+        if self.pack_pro_id:
+            self.uni_med = self.pack_pro_id.uom_id
+
+    @api.constrains('pack_pro_id')
+    @api.one
+    def _check_pack_pro(self):
+        if self.pack_pro_id.pack == True:
+            raise ValidationError(_('ERROR: El Paquete No Debe Contener Productos Declarados Como Paquetes'))
+
+class product_template(models.Model):
+    _name = 'product.template'
+    _inherit = 'product.template'
+    pack = fields.Boolean('Paquete')
+    tip = fields.Boolean('Precio Basado en Componetes')
+    mod_pro = fields.Boolean('Mod', default=True)
+
+    pro_ids = fields.One2many('pack.product', 'pro_id', 'Productos')
+
+    @api.depends('pro_ids')
+    @api.one
+    def calcula_product(self):
+        if self.tip == True:
+            acum = 0.0
+            for xcal in self.pro_ids:
+                acum+= xcal.pack_pro_id.list_price*xcal.subtotal_price
+            if acum:
+                self.pre_sale = acum
+                self.list_price = 0.0
+
+    pre_sale = fields.Float('Precio del Paquete', digits=(14,2), compute="calcula_product")
+
+    @api.onchange('pack')
+    def onchange_pack(self):
+        if self.pack == False:
+            prod_list = []
+            self.update({'pro_ids':prod_list})
+
+    @api.onchange('tip')
+    def onchange_sale(self):
+        if self.tip == True:
+            acum = 0.0
+            for xcal in self.pro_ids:
+                acum+= xcal.pack_pro_id.list_price*xcal.subtotal_price
+            if acum:
+                self.pre_sale = acum
+                self.list_price = 0.0
+
+    @api.constrains('list_price')
+    @api.one
+    def _check_list_price(self):
+        if self.pack == True:
+            if self.tip == True:
+                if self.list_price != 0:
+                    raise ValidationError(_('El Paquete No Debe Tener Valor Si Esta Basado En Precio De Sus Componentes'))
+
+    @api.constrains('pro_ids')
+    @api.one
+    def _check_categ(self):
+        print "####### SELF CATEGORY >>>>>> ", self.uom_id.category_id.name
+        for item in self.pro_ids:
+            print "##### UNIDAD >>>>>> ", item.uni_med.name
+            print "##### CATEGORIA >>>>>> ", item.uni_med.category_id.name
+            if self.uom_id.category_id.name != item.uni_med.category_id.name:
+                raise ValidationError(_('La Categoria de la Unidad De Medida del Producto:\n %s \nDebe ser igual a la Categoria de la Unidad del Paquete'% item.pack_pro_id.name))
+
+    @api.constrains('pro_ids')
+    @api.one
+    def _check_pack(self):
+        if self.pack == True:
+            acum = 0.0
+            for pval in self.pro_ids:
+                acum+= pval.pack_pro_id.list_price
+            if acum == 0.0:
+                raise ValidationError(_('El Paquete Debe Contener Algun Producto'))
+
+    _order = 'name'
+    _defaults = {
+        'active': True,
+       }
